beae650a3b8268659ea96cc2511b9860d5e0b05a
mahmoud1xxxx1-bit <mahmoud1xxxx1@gmail.com>
2026-08-16 01:53:28 +0300

    feat(accounting): standardize core accounting terminology

---
+++ lib/features/accounting_notebook/utils/notebook_terminology.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/widgets.dart';
+
+class NotebookTerminology {
+  const NotebookTerminology._();
+
+  static bool _isArabic(BuildContext context) =>
+      Localizations.localeOf(context).languageCode == 'ar';
+
+  static String accountsReceivable(BuildContext context) =>
+      _isArabic(context) ? 'الذمم المدينة (لي)' : 'Accounts Receivable';
+
+  static String accountsPayable(BuildContext context) =>
+      _isArabic(context) ? 'الذمم الدائنة (عليّ)' : 'Accounts Payable';
+
+  static String totalAccountsReceivable(BuildContext context) =>
+      _isArabic(context)
+          ? 'إجمالي الذمم المدينة (لي)'
+          : 'Total Accounts Receivable';
+
+  static String totalAccountsPayable(BuildContext context) =>
+      _isArabic(context)
+          ? 'إجمالي الذمم الدائنة (عليّ)'
+          : 'Total Accounts Payable';
+
+  static String receivePayment(BuildContext context) =>
+      _isArabic(context) ? 'تحصيل' : 'Receive Payment';
+
+  static String makePayment(BuildContext context) =>
+      _isArabic(context) ? 'سداد' : 'Make Payment';
+
+  static String totalAccountBalance(BuildContext context) =>
+      _isArabic(context) ? 'إجمالي أرصدة الحسابات' : 'Total Account Balance';
+
+  static String receivablesPayablesSection(BuildContext context) =>
+      _isArabic(context)
+          ? 'الذمم والحسابات'
+          : 'Receivables, Payables & Accounts';
+}
